bde1f2eb8d51ccc3e7abda556d402244ebb32a43
Bainazarov Ramadan <romabainazar@gmail.com>
2024-03-02 13:09:36 +0600

    new

---
+++ pkg/OCM/model/students.go
@@ -0,0 +1,95 @@
+package model
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Student struct {
+	StudentID int    `json:"student_id"`
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	GPA       float64 `json:"gpa"`
+}
+
+var students = []Student{
+	{
+		StudentID: 1,
+		Name:      "John Doe",
+		Age:       20,
+		GPA:       3.8,
+	},
+	{
+		StudentID: 2,
+		Name:      "Jane Smith",
+		Age:       22,
+		GPA:       3.9,
+	},
+	{
+		StudentID: 3,
+		Name:      "Alice Johnson",
+		Age:       21,
+		GPA:       3.7,
+	},
+}
+
+func GetStudents() []Student {
+	return students
+}
+
+type StudentModel struct {
+	DB *sql.DB
+}
+
+func (sm *StudentModel) Get(id int) (*Student, error) {
+	for _, s := range students {
+		if s.StudentID == id {
+			return &s, nil
+		}
+	}
+	return nil, errors.New("Student not found")
+}
+
+func (sm *StudentModel) Insert(student *Student) error {
+	
+	query := `
+		INSERT INTO students (student_id, name, age, gpa) 
+		VALUES ($1, $2, $3, $4) 
+		RETURNING student_id
+	`
+	args := []interface{}{student.StudentID, student.Name, student.Age, student.GPA}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return sm.DB.QueryRowContext(ctx, query, args...).Scan(&student.StudentID)
+}
+
+func (sm *StudentModel) Update(student *Student) error {
+	
+	query := `
+        UPDATE students
+        SET name = $1, age = $2, gpa = $3
+        WHERE student_id = $4
+        RETURNING student_id
+    `
+	args := []interface{}{student.Name, student.Age, student.GPA, student.StudentID}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return sm.DB.QueryRowContext(ctx, query, args...).Scan(&student.StudentID)
+}
+
+func (sm *StudentModel) Delete(id int) error {
+
+	query := `
+        DELETE FROM students
+        WHERE student_id = $1
+    `
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := sm.DB.ExecContext(ctx, query, id)
+	return err
+}
